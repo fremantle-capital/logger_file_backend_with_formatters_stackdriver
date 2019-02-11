5af6f0de7747be3ff84ccddc76fe35ad6a09c6ab
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-02-11 14:30:15 -0800

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule LoggerFileBackendWithFormattersStackdriver.MixProject do
       version: "0.0.1",
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -17,10 +19,26 @@ defmodule LoggerFileBackendWithFormattersStackdriver.MixProject do
     ]
   end
 
+  defp description do
+    "Google Cloud Stackdriver formatter for :logger_file_backend_with_formatters"
+  end
+
+  defp package do
+    [
+      maintainers: ["Alex Kwiatkowski"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" =>
+          "https://github.com/fremantle-capital/logger_file_backend_with_formatters_stackdriver"
+      }
+    ]
+  end
+
   defp deps do
     [
       {:logger_file_backend_with_formatters, "~> 0.0.1"},
-      {:jason, "~> 1.1"}
+      {:jason, "~> 1.1"},
+      {:ex_doc, "~> 0.19", only: :dev}
     ]
   end
 end
